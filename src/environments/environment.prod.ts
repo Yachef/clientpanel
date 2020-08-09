a6fbcf660db51db943f31210f5d30c98ebba6ff8
Yachef <yachef.h@gmail.com>
2020-08-09 19:00:02 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA3Lo0su49ToUvNhNwb2BDINYA6p_Wm1CI",
+    authDomain: "clientpanelprod-bf6a7.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-bf6a7.firebaseio.com",
+    projectId: "clientpanelprod-bf6a7",
+    storageBucket: "clientpanelprod-bf6a7.appspot.com",
+    messagingSenderId: "511396466993",
+    appId: "1:511396466993:web:a085fe5a19e15e508a6010"
+  }
 };
